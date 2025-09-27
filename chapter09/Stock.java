3cedd7b9f7aa5f2c0846ef3b91e06c4a23d57abc
WeiquanMai <weiquanmai@gmail.com>
2025-09-27 12:41:15 -0400

    Create Stock.java

---
+++ chapter09/Stock.java
@@ -0,0 +1,24 @@
+public class Stock {
+    private String symbol;
+    private String name;
+    private double previousClosingPrice;
+    private double currentPrice;
+
+    public Stock(String symbol, String name){
+        this.symbol = symbol;
+        this.name = name;
+    }
+
+    // Setters
+    public void setPreviousClosingPrice(double previousClosingPrice){
+        this.previousClosingPrice = previousClosingPrice;
+    }
+    public void setCurrentPrice(double currentPrice){
+        this.currentPrice = currentPrice;
+    }
+
+    public double getChangePercent(){
+
+        return ((currentPrice - previousClosingPrice) / previousClosingPrice) * 100;
+    }
+}
