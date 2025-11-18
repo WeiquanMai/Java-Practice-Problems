f32d17e22b2b356e974bb906c664a010424b251d
WeiquanMai <weiquanmai@gmail.com>
2025-11-17 20:30:56 -0500

    Add Exercise01_01 to display Java welcome messages

---
+++ chapter01/Exercise01_01.java
@@ -0,0 +1,13 @@
+/**
+ * @Author Weiquan Mai
+ * Problem 1.1
+ * Write a program that displays Welcome to Java, Learning Java Now, and Programming is fun.
+ */
+
+public class Exercise01_01{
+    public static void main(String[] args){
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programming is fun");
+    }
+}
