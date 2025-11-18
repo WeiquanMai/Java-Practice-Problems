13be01d4ffd75269fa9b3fcd8f63195eb85794a0
WeiquanMai <weiquanmai@gmail.com>
2025-11-17 21:07:51 -0500

    Add Exercise01_05 to compute an expression
    Implement program to compute a specific expression.

---
+++ chapter01/Exercise01_05.java
@@ -0,0 +1,12 @@
+/**
+ * @Author Weiquan Mai
+ * Problem 1.5
+ * (Compute expressions)
+ * Write a program that displays the result of 7.5 X 6.5 - 4.5 X 3/ 47.5 - 5.5
+ */
+public class Exercise01_05 {
+    public static void main(String[] args){
+        double result = (7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5);
+        System.out.println("Result: " + result);
+    }
+}
