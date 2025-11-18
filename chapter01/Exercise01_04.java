08a97942a52ccbbacb1a13cea28783826a9c34d8
WeiquanMai <weiquanmai@gmail.com>
2025-11-17 20:50:46 -0500

    Add Exercise01_04
    Implement a program to print a table of values.

---
+++ chapter01/Exercise01_04.java
@@ -0,0 +1,14 @@
+/**
+ * @Author Weiquan Mai
+ * Problem 1.4
+ * (Print a table)
+ * Write a program that displays a table
+ */
+public class Exercise01_04 {
+    public static void main(String[] args){
+        System.out.println("a\t a^2\t a^3\t a^4");
+        System.out.println("1\t 1\t\t 1\t\t 1");
+        System.out.println("2\t 4\t\t 8\t\t 16");
+        System.out.println("4\t 16\t\t 64\t\t 256");
+    }
+}
