0846424b9393a905e6fd8c07d099c90bf14be6df
WeiquanMai <weiquanmai@gmail.com>
2025-11-17 20:34:17 -0500

    Add Exercise01_02
    This program displays the message 'I love Java' five times.

---
+++ chapter01/Exercise01_02.java
@@ -0,0 +1,15 @@
+/**
+ * @Author Weiquan Mai
+ * Problem 1.2
+ * (Display five messages)
+ * Write a program that displays I love Java five times
+ */
+public class Exercise01_02 {
+    public static void main(String[] args){
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+    }
+}
