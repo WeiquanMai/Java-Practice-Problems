186495a614571a37f7638f2ec15c62c8b7fd31fe
WeiquanMai <weiquanmai@gmail.com>
2025-11-21 16:49:13 -0500

    Add problem number

--- chapter02/Exercise02_19.java
+++ chapter02/Exercise02_19.java
@@ -1,5 +1,6 @@
 /**
  * @Author Weiquan Mai
+ * Problem 2.19
  * (Geometry: area of a triangle)
  * Write a program that prompts the user to enter three points,
  * (x1, y1), (x2, y2), (x3, y3), of a triangle and then displays its area.
