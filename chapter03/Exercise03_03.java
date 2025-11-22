16e0cdddfa215707d975e16f674d23ba36dfad66
WeiquanMai <weiquanmai@gmail.com>
2025-11-21 20:14:22 -0500

    Add Exercise03_03 for solving linear equations
    Implement a program to solve 2x2 linear equations using Cramer's rule.

---
+++ chapter03/Exercise03_03.java
@@ -0,0 +1,43 @@
+/**
+ * @Author Weiquan Mai
+ * Problem 3.3
+ * (Algebra: solve 2 X 2 linear equations)
+ * A linear equation can be solved using Cramer's rule given in Programming Exercise 1.13.
+ * Write a program that prompts the user to enter a, b, c, d, e, and f and displays the result.
+ * If ad - bc is 0, report that "The equation has no solution".
+ */
+import java.util.Scanner;
+
+public class Exercise03_03 {
+    public static void main(String[] args){
+        // Create a Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Ask for user input
+        System.out.print("Enter a, b, c, d, e, f: ");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        double d = input.nextDouble();
+        double e = input.nextDouble();
+        double f = input.nextDouble();
+
+        // Calculate x and y
+        double x = ((e * d) - (b * f)) / ((a * d) - (b * c));
+        double y = ((a * f) - (e * c)) / ((a * d) - (b * c));
+
+        // Calculate determinant
+        double determinant = (a * d) - (b * c);
+
+        // Display result
+        if(determinant == 0){
+            System.out.println("The equation has no solution");
+        }
+        else{
+            System.out.println("x is " + x + " and y is " + y);
+        }
+
+        // Close scanner
+        input.close();
+    }
+}
