7d52bb8325d448cdc4ecb4376b71fb3b8440cbe3
WeiquanMai <weiquanmai@gmail.com>
2026-01-28 14:24:47 -0500

    Implement MyDate class with date handling features
    The MyDate class includes constructors for current date, elapsed time, and specified date. It also provides getter methods and a method to set the date using elapsed time.

---
+++ chapter10/MyDate.java
@@ -0,0 +1,69 @@
+import java.util.GregorianCalendar;
+
+/**
+ * @author Weiquan Mai
+ * Problem 10.14
+ * (The MyDate class)
+ * Design a class named MyDate. The class contains:
+ * ■■ The data fields year, month, and day that represent a date. month is 0-based,
+ * i.e., 0 is for January.
+ * ■■ A no-arg constructor that creates a MyDate object for the current date.
+ * ■■ A constructor that constructs a MyDate object with a specified elapsed time
+ * since midnight, January 1, 1970, in milliseconds.
+ * ■■ A constructor that constructs a MyDate object with the specified year, month,
+ * and day.
+ * ■■ Three getter methods for the data fields year, month, and day, respectively.
+ * ■■ A method named setDate(long elapsedTime) that sets a new date for
+ * the object using the elapsed time.
+ * Write a test program that creates two MyDate objects (using new MyDate() and new
+ * MyDate(34355555133101L)) and displays their year, month, and day.
+ * (Hint: The first two constructors will extract the year, month, and day
+ * from the elapsed time. For example, if the elapsed time is 561555550000
+ * milliseconds, the year is 1987, the month is 9, and the day is 18. You may
+ * use the GregorianCalendar class discussed in Programming Exercise 9.5
+ * to simplify coding.)
+ */
+class MyDate {
+    // Variables
+    private int year;
+    private int month;
+    private int date;
+
+    // No-arg constructor
+    public MyDate(){
+        GregorianCalendar calendar = new GregorianCalendar();
+        this.year = calendar.get(GregorianCalendar.YEAR);
+        this.month = calendar.get(GregorianCalendar.MONTH);
+        this.date = calendar.get(GregorianCalendar.DAY_OF_MONTH);
+    }
+    // Overloaded constructor
+    public MyDate(long millis){
+        setDate(millis);
+    }
+    public MyDate(int year, int month, int date){
+        this.year = year;
+        this.month = month;
+        this.date = date;
+    }
+    // Accessor methods
+    public int getYear(){
+        return this.year;
+    }
+    public int getMonth(){
+        return this.month;
+    }
+    public int getDate(){
+        return this.date;
+    }
+    /**
+     * A method named setDate(long elapsedTime) that sets a new date
+     * for the object using the elapsed time.
+     */
+    public void setDate(long elapsedTime){
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(elapsedTime);
+        this.year = calendar.get(GregorianCalendar.YEAR);
+        this.month = calendar.get(GregorianCalendar.MONTH);
+        this.date = calendar.get(GregorianCalendar.DATE);
+    }
+}
